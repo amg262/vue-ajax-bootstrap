aba4eca43f318d5be9ddbe5ce52bc0f369dfd7ec
Andrew <andrewmgunn26@gmail.com>
2022-03-30 14:42:29 -0500

    updates

--- js/app-models.js
+++ js/app-models.js
@@ -1,28 +1,28 @@
 // creates an array with extra methods for our Book Collections
-const BookCollection = function(arr){
+const BookCollection = function (arr) {
     // Load array if provided.
     // Load empty array if one was not provided.
-    if(!Array.isArray(arr)){
+    if (!Array.isArray(arr)) {
         arr = [];
     }
 
     // Methods for using the collection.
-    arr.add = function(book){
+    arr.add = function (book) {
         return arr.push(book);
     };
 
-    arr.remove = function(book){
+    arr.remove = function (book) {
         return arr.splice(arr.findBook(book), 1);
     };
 
-    arr.contains = function(book){
+    arr.contains = function (book) {
         return this.findBook(book) >= 0;
     };
 
     // Internal function for finding books.
     // Assumes all books have an id.
-    arr.findBook = function(book){
-        return arr.findIndex(function(item){
+    arr.findBook = function (book) {
+        return arr.findIndex(function (item) {
             return item.id == book.id;
         });
     };
